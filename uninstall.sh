cf24a56b2560a9dee8eb3839d295f02df0da83a8
Joseph Mory <morytyann@outlook.com>
2024-11-13 21:47:39 +0800

    use a better way to uninstall

--- uninstall.sh
+++ uninstall.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 
 if [ -x "/bin/opkg" ]; then
-	opkg remove mihomo --force-removal-of-dependent-packages
+	opkg remove luci-i18n-mihomo-zh-cn
+	opkg remove luci-app-mihomo
+	opkg remove mihomo
 elif [ -x "/usr/bin/apk" ]; then
+	apk del luci-i18n-mihomo-zh-cn
+	apk del luci-app-mihomo
 	apk del mihomo
 fi
 
