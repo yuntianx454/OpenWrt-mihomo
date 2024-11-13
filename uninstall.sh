a27100827f01522c0ee196513b828a5ef76c1ce9
Joseph Mory <morytyann@outlook.com>
2024-11-13 21:45:16 +0800

    compatiable with apk

--- uninstall.sh
+++ uninstall.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
-opkg remove mihomo --force-removal-of-dependent-packages
+if [ -x "/bin/opkg" ]; then
+	opkg remove mihomo --force-removal-of-dependent-packages
+elif [ -x "/usr/bin/apk" ]; then
+	apk del mihomo
+fi
+
 rm -rf /etc/mihomo
 rm -f /etc/config/mihomo
